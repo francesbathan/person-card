78bd0f9a4ed607e729e0608744d146723abac235
frances <frncsbthn@gmail.com>
2020-03-04 13:45:23 -0800

    converted into functional components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from "react";
 // import logo from "./logo.svg";
 import "./App.css";
-import PersonCard from "./components/PersonCard";
+// import PersonCard from "./components/PersonCard";
 // import Button from "./components/Button";
+import PersonCard2 from "./components/PersonCard2";
 
 function App() {
   return (
     <div className="App">
-      <PersonCard firstName="Jane" lastName="Doe" age={45} hairColor="Black" />
-      <PersonCard
+      <PersonCard2 firstName="Jane" lastName="Doe" age={45} hairColor="Black" />
+      <PersonCard2
         firstName="John"
         lastName="Smith"
         age={88}
         hairColor="Brown"
       />
-      <PersonCard
+      <PersonCard2
         firstName="Millard"
         lastName="Filmore"
         age={50}
         hairColor="Brown"
       />
-      <PersonCard
+      <PersonCard2
         firstName="Maria"
         lastName="Smith"
         age={62}
